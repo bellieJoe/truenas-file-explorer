343e6c994674b1a142f38d30f932633a9c0f7df6
Bellie Joe <jandusayjoe14@gmail.com>
2023-01-11 15:53:35 +0800

    prevent node from crashing

--- express-backend/app.js
+++ express-backend/app.js
@@ -5,23 +5,50 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
+  
+app.get('/download', (req, res, next) => {
+    try {
+        const c = new ftp();
+    
+        c.on('ready', function() {
+            res.setHeader('Content-Type', req.query.mimetype);
+            res.setHeader('Content-disposition', 'attachment; filename=' + path.basename(req.query.file));
+            c.get(req.query.file, function(err, stream) {
+                console.log("ok")
+                if (err) throw err;
+                stream.once('close', function() { c.end(); });
+                stream.pipe(res);
+            });
+        });
 
-app.get('/download', (req, res) => {
-
-  const c = new ftp();
-
-  c.on('ready', function() {
-    res.setHeader('Content-Type', req.query.mimetype);
-    res.setHeader('Content-disposition', 'attachment; filename=' + path.basename(req.query.file));
-    c.get(req.query.file, function(err, stream) {
-      if (err) throw err;
-      stream.once('close', function() { c.end(); });
-      stream.pipe(res);
-    });
-  });
-  //connect to the ftp server
-  c.connect({ host: req.query.host, user: req.query.username, password: req.query.password });
+        //connect to the ftp server
+        c.connect({ host: req.query.host, user: req.query.username, password: req.query.password });
+    } catch (error) {
+        // throw(error)
+    }
+    
+ 
 });
+
+app.get('', (req, res, next) => {
+    throw "ok";
+    res.send("ko")
+})
+
+process.on('uncaughtException', function (err) {
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+    console.error(err.stack)
+});
+
+process.on('unhandledRejection', (reason, p) => {
+    console.error((new Date).toUTCString() + ' Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.status(500).send('Something broke!');
+});
+
 
 const host = 'localhost';
 app.listen(3000, host, () => {
